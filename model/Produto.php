6e2a15082ea10cf64836d6b7fed0851ffa28ff7a
lucas <moraislcs05@gmail.com>
2025-07-11 19:52:45 -0300

    adding getImagemUrl

--- model/Produto.php
+++ model/Produto.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/Entidade.php';
+
 class Produto extends Entidade
 {
     private string $nome;
@@ -9,7 +11,8 @@ class Produto extends Entidade
 
     public function __construct(
         ?int $id, string $nome, ?string $descricao, float $preco, ?Categoria $categoria, bool $ativo = true, 
-        ?string $dataCriacao = null, ?string $dataAtualizacao = null, ?Usuario $usuarioAtualizacao = null
+        ?string $dataCriacao = null, ?string $dataAtualizacao = null, ?Usuario $usuarioAtualizacao = null,
+        ?string $imagemUrl = null
     ) {
         parent::__construct($id, $ativo, $dataCriacao, $dataAtualizacao, $usuarioAtualizacao);
         $this->nome = $nome;
@@ -22,4 +25,9 @@ class Produto extends Entidade
     public function getDescricao(): ?string { return $this->descricao; }
     public function getPreco(): float { return $this->preco; }
     public function getCategoria(): ?Categoria { return $this->categoria; }
+
+    public function getImagemUrl(): string 
+    {
+        return $this->imagemUrl ?? 'https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg';
+    }
 }
